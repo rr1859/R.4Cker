9373ad404d186c90ea9f97fd6e87073bb42d4f75
Ramya Raviram <ramya617@gmail.com>
2016-04-18 12:14:27 -0400

    Update sam_bedGraph.sh
    change awk < $file '{print $4}' to awk < $file '{print $3}'

--- sam_bedGraph.sh
+++ sam_bedGraph.sh
@@ -2,4 +2,4 @@
 
 directory=$1
 cd $directory
-for file in *aligned.sam ; do awk < $file '{print $4}' | grep "^ch" | sort | uniq -c | sed -e 's/_\|:\| \|-/\t/g' |  awk '{print $2"\t"$3"\t"$4"\t"$1 }' - >  $(echo $file | sed 's/.sam/.bedGraph/g') ; done
+for file in *aligned.sam ; do awk < $file '{print $3}' | grep "^ch" | sort | uniq -c | sed -e 's/_\|:\| \|-/\t/g' |  awk '{print $2"\t"$3"\t"$4"\t"$1 }' - >  $(echo $file | sed 's/.sam/.bedGraph/g') ; done
