5131577da90b7ba1e867b4c13dab8c0be32db62e
Ramya Raviram <ramya617@gmail.com>
2018-07-22 16:24:13 -0700

    Update merge_windows.R
    merge only if nrow(windows) >2

--- R/merge_windows.R
+++ R/merge_windows.R
@@ -4,16 +4,20 @@ merge_windows = function(windows){
   chr=as.character(windows[1,1])
   start=windows[1,2]
   end=windows[1,3]
-  for(i in 2:nrow(windows)){
-    if(as.character(windows[i,1]) == chr & windows[i,2] <= end & windows[i,2] >= start){
-      end=windows[i,3]
+  if(nrow(windows) > 2){
+    for(i in 2:nrow(windows)){
+      if(as.character(windows[i,1]) == chr & windows[i,2] <= end & windows[i,2] >= start){
+        end=windows[i,3]
+      }
+      else{
+        output=rbind(output, c(chr,start, end))
+        chr=as.character(windows[i,1])
+        start=windows[i,2]
+        end=windows[i,3]
+      }
     }
-    else{
-      output=rbind(output, c(chr,start, end))
-      chr=as.character(windows[i,1])
-      start=windows[i,2]
-      end=windows[i,3]
-    }
+    return(data.frame(output))
+  } else{
+    return(windows)
   }
-  return(data.frame(output))
 }
